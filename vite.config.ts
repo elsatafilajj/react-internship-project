82562374ade1d2be8cc1f1ab6e45b0b475ccb8c7
elsatafilajj <elsatafilaj11@gmail.com>
2025-04-30 16:15:14 +0200

    add shadcn theme

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 8000,
-  },
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Tailwind CSS plugin for Vite
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
