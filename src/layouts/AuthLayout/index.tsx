c27af4833054750d5dfa19a2be219c3dfb3522da
elsatafilajj <elsatafilaj11@gmail.com>
2025-05-02 15:20:56 +0200

    feat: design signup page with tailwind css and shadcn/ui

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -2,19 +2,13 @@ import { Outlet } from 'react-router-dom';
 
 import { RouteWrapper } from '@/routes/RouteWrapper';
 
-import './AuthLayout.css';
+// import './AuthLayout.css';
 
 export const AuthLayout = () => {
   return (
     <RouteWrapper isAuthenticationPage>
-      <div className="auth-layout">
-        <div className="auth-layout__bg-image" />
-
-        <div className="auth-layout__form-container">
-          <div className="auth-layout__form">
-            <Outlet />
-          </div>
-        </div>
+      <div className="bg-gradient-to-b from-background to-white dark:bg-gray-100">
+        <Outlet />
       </div>
     </RouteWrapper>
   );
