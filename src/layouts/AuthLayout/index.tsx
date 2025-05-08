0d7cde4bbb5df7c9bfebe2983136c12567185dc7
EraaIbrahimi <era@kutia.net>
2025-05-08 15:49:49 +0200

    fix/remove manual dark class on authlayout

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -6,7 +6,7 @@ import { RouteWrapper } from '@/routes/RouteWrapper';
 export const AuthLayout = () => {
   return (
     <RouteWrapper isAuthenticationPage>
-      <div className="bg-gradient-to-b from-background to-secondary dark">
+      <div className="bg-gradient-to-b from-background to-secondary">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
           <div className="max-w-[470px] w-full rounded-2xl shadow md:mt-0 xl:p-0">
             <div className="p-6 space-y-4 md:space-y-10 sm:p-8 bg-card rounded-2xl flex flex-col">
