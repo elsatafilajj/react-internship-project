ffd3a61bb8ca3879c49df3438656b36e488fd560
EraaIbrahimi <era@kutia.net>
2025-05-12 17:23:53 +0200

    fix: get logo component generated from svgr

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import LogoFull from '@/assets/icons/general/LogoFull';
+import { LogoFullIcon } from '@/assets/icons/general/LogoFull';
 import { RouteWrapper } from '@/routes/RouteWrapper';
 
 export const AuthLayout = () => {
@@ -10,7 +10,7 @@ export const AuthLayout = () => {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
           <div className="max-w-[470px] w-full rounded-2xl shadow md:mt-0 xl:p-0">
             <div className="p-6 space-y-4 md:space-y-10 sm:p-8 bg-card rounded-2xl flex flex-col">
-              <LogoFull />
+              <LogoFullIcon />
               <Outlet />
 
               <p className="text-center text-xs text-foreground">
