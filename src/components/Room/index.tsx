1f1851e89b5981dc6879cd4d0431f31923b93330
elsatafilajj <elsatafilaj11@gmail.com>
2025-05-14 15:44:10 +0200

    fix/add style to Rooms page and Room component

--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -7,13 +7,13 @@ interface RoomProps {
 
 export const Room = ({ title, createdAt }: RoomProps) => {
   return (
-    <div className="w-[350px] rounded-3xl justify-center border shadow-lg  overflow-hidden bg-white">
-      <div className="h-[150px] bg-gray-100 flex items-center justify-center">
+    <div className="w-full max-w-sm rounded-3xl border shadow-lg bg-white">
+      <div className="h-[150px] bg-gray-100 flex items-center rounded-t-3xl justify-center">
         <span className="text-gray-400">Preview Page</span>
       </div>
 
       <div className="flex items-start gap-2 p-4">
-        <StickyNote className="text-primary w-6 h-6 mt-1 items-center" />
+        <StickyNote className="text-primary w-6 h-6 mt-1" />
         <div className="text-gray-800">
           <p className="font-semibold text-lg">{title}</p>
           <p className="text-sm text-gray-500">
--- src/pages/Rooms/index.tsx
+++ src/pages/Rooms/index.tsx
@@ -9,20 +9,26 @@ export const Rooms = () => {
     queryFn: () => roomTableData,
   });
 
-  if (isLoading) return <p>Loading rooms...</p>;
-  if (isError) {
-    return <p>Error: {(error as Error).message}</p>;
-  }
+  if (isLoading)
+    return <p className="text-center mt-10 text-gray-600">Loading rooms...</p>;
+  if (isError)
+    return (
+      <p className="text-center mt-10 text-red-500">
+        Error: {(error as Error).message}
+      </p>
+    );
 
   return (
-    <div className="flex flex-wrap gap-5 m-10">
-      {data!.map((roomData) => (
-        <Room
-          key={roomData.room.id}
-          title={roomData.room.title}
-          createdAt={roomData.room.createdAt}
-        />
-      ))}
+    <div className="px-4 py-8 max-w-8xl rounded-3xl mx-auto">
+      <div className="grid gap-6 m-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data?.map((roomData) => (
+          <Room
+            key={roomData.room.id}
+            title={roomData.room.title}
+            createdAt={roomData.room.createdAt}
+          />
+        ))}
+      </div>
     </div>
   );
 };
