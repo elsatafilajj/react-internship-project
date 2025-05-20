b3b97f9639f6b57ff92fa6802b835818eb2e4819
elsatafilajj <elsatafilaj11@gmail.com>
2025-05-20 11:24:40 +0200

    feat/fetch all notes by roomId

--- src/pages/RoomDetails/index.tsx
+++ src/pages/RoomDetails/index.tsx
@@ -1,15 +1,28 @@
 import { useParams } from 'react-router-dom';
 
-import { useGetRoomByIdQuery } from '@/api/Room/room.queries';
+import { useGetAllNotesFromRoomQuery } from '@/api/Note/notes.queries';
+import { Note } from '@/components/Note';
 
 export const RoomDetails = () => {
   const { roomId } = useParams<{ roomId: string }>();
-  const { data, isLoading, isError, error } = useGetRoomByIdQuery(roomId || '');
 
-  if (isLoading) return <p>Loading room dashboard...</p>;
+  const {
+    data: notes,
+    isLoading,
+    isError,
+    error,
+  } = useGetAllNotesFromRoomQuery(roomId || '');
 
+  console.log('test', notes);
+
+  if (isLoading)
+    return <p className="text-center mt-10 text-gray-600">Loading notes...</p>;
   if (isError)
-    return <p className="text-red-500">Error: {(error as Error).message}</p>;
+    return (
+      <p className="text-center mt-10 text-red-500">
+        Error: {(error as Error).message}
+      </p>
+    );
 
-  return <div>Dashboard for: {data?.data.title}</div>;
+  return <div>{notes?.data.map((note) => <Note key={note.uuid} />)}</div>;
 };
