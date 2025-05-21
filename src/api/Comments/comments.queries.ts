28a3594c5acaf3c0e962f6e3c48f00644d09d66a
elsatafilajj <elsatafilaj11@gmail.com>
2025-05-21 21:22:42 +0200

    fix/fix bug on comments.queries.ts file

--- src/api/Comments/comments.queries.ts
+++ src/api/Comments/comments.queries.ts
@@ -2,14 +2,14 @@ import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import { getAllCommentsForNote } from '@/api/Comments/comments.client';
-import { CommentsResponse } from '@/api/Comments/comments.types';
+import { NoteComment } from '@/api/Comments/comments.types';
 import { queryKeys } from '@/constants/queryKeys';
 
 export const useGetAllCommentsQuery = (
   noteId: string,
-  options?: UseQueryOptions<AxiosResponse<CommentsResponse[]>>,
+  options?: UseQueryOptions<AxiosResponse<NoteComment[]>>,
 ) => {
-  return useQuery<AxiosResponse<CommentsResponse[]>>({
+  return useQuery<AxiosResponse<NoteComment[]>>({
     queryKey: queryKeys.getCommentsByNoteId(noteId),
     queryFn: () => getAllCommentsForNote(noteId),
     enabled: !!noteId,
