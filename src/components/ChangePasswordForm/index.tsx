ebad7a83a96b2afc19c6f2acf8a383530f30a399
elsatafilajj <elsatafilaj11@gmail.com>
2025-05-26 13:22:33 +0200

    feat/add change password functionality in the user profile

--- src/components/ChangePasswordForm/index.tsx
+++ src/components/ChangePasswordForm/index.tsx
@@ -1,3 +1,7 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+import { changePassword } from '@/api/User/user.client';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -8,8 +12,46 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import { queryKeys } from '@/constants/queryKeys';
+import { useAuthContext } from '@/context/AuthContext/AuthContext';
+import { getFormikError } from '@/helpers/getFormikError';
+import { useForm } from '@/hooks/useForm';
+import { ChangePasswordSchema } from '@/schemas/ChangePasswordSchema';
 
-export const ChangePasswordForm = () => {
+interface ChangePasswordFormProps {
+  id: string;
+}
+
+export const ChangePasswordForm = ({ id }: ChangePasswordFormProps) => {
+  const { logout } = useAuthContext();
+  const queryClient = useQueryClient();
+
+  const changePasswordMutation = useMutation({
+    mutationFn: changePassword,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.getSingleUser(id) });
+      toast.success('Password has changed!');
+      logout();
+    },
+  });
+
+  const formik = useForm({
+    schema: ChangePasswordSchema,
+    initialValues: {
+      oldPassword: '',
+      newPassword: '',
+      newPasswordConfirm: '',
+    },
+    onSubmit: async (values, formikHelpers) => {
+      try {
+        await changePasswordMutation.mutateAsync(values);
+        formikHelpers.resetForm();
+      } catch {
+        console.error('Change password failed');
+      }
+    },
+  });
+
   return (
     <Card>
       <CardHeader className="text-center">
@@ -19,31 +61,42 @@ export const ChangePasswordForm = () => {
         </CardDescription>
       </CardHeader>
 
-      <CardContent className="space-y-4 mt-4">
-        <Input
-          name="oldPassword"
-          type="password"
-          placeholder="Enter your old password"
-          id="oldPassword"
-        />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Enter your new password"
-          id="password"
-        />
-        <Input
-          name="passwordConfirm"
-          type="password"
-          placeholder="Confirm new password"
-          id="passwordConfirm"
-        />
-      </CardContent>
-      <CardFooter>
-        <Button type="submit" className="mt-8">
-          Save changes
-        </Button>
-      </CardFooter>
+      <form onSubmit={formik.handleSubmit} className="space-y-2">
+        <CardContent className="space-y-4 mt-4">
+          <Input
+            name="oldPassword"
+            type="password"
+            placeholder="Enter your old password"
+            id="oldPassword"
+            value={formik.values.oldPassword}
+            onChange={formik.handleChange}
+            error={getFormikError(formik, 'oldPassword')}
+          />
+          <Input
+            name="newPassword"
+            type="password"
+            placeholder="Enter your new password"
+            id="newPassword"
+            value={formik.values.newPassword}
+            onChange={formik.handleChange}
+            error={getFormikError(formik, 'newPassword')}
+          />
+          <Input
+            name="newPasswordConfirm"
+            type="password"
+            placeholder="Confirm new password"
+            id="newPasswordConfirm"
+            value={formik.values.newPasswordConfirm}
+            onChange={formik.handleChange}
+            error={getFormikError(formik, 'newPasswordConfirm')}
+          />
+        </CardContent>
+        <CardFooter>
+          <Button type="submit" className="mt-8" disabled={formik.isSubmitting}>
+            {formik.isSubmitting ? 'Saving...' : 'Save changes'}
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 };
