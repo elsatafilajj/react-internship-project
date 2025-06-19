048169b67e2c1de86e5365fbbf6479a461777f49
elsatafilajj <elsatafilaj11@gmail.com>
2025-06-19 15:49:33 +0200

    feat/implement bulleted list for text in notes

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { Circle, Crown, MessageSquare, Star, X } from 'lucide-react';
+import { Circle, Crown, List, MessageSquare, Star, X } from 'lucide-react';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
@@ -67,6 +67,8 @@ export const Note = ({ note, isReadOnly }: NoteProps) => {
   const isUserVoter = note.noteVotes?.find(
     (item) => item.user.uuid === user?.uuid,
   );
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
   const [hasVoted, setHasVoted] = useState<boolean>(!!isUserVoter);
 
   const debouncedContent: string = useDebounce(noteContent, 1000);
@@ -78,6 +80,81 @@ export const Note = ({ note, isReadOnly }: NoteProps) => {
   useEffect(() => {
     if (content) setNoteContent(content);
   }, [content]);
+
+  const handleEnterKey = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key !== 'Enter') return;
+
+    const textarea = textareaRef.current;
+    if (!textarea) return;
+
+    const { selectionStart } = textarea;
+    const text = noteContent;
+    const lines = text.split('\n');
+
+    let charCount = 0;
+    let lineIndex = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+      if (charCount + lines[i].length + 1 > selectionStart) {
+        lineIndex = i;
+        break;
+      }
+      charCount += lines[i].length + 1;
+    }
+
+    const currentLine = lines[lineIndex];
+
+    if (currentLine.startsWith('• ')) {
+      e.preventDefault();
+
+      const before = text.substring(0, selectionStart);
+      const after = text.substring(selectionStart);
+
+      const updated = `${before}\n• ${after}`;
+      setNoteContent(updated);
+
+      setTimeout(() => {
+        const newPos = selectionStart + 3;
+        textarea.setSelectionRange(newPos, newPos);
+      }, 0);
+    }
+  };
+
+  const toggleBulletOnCurrentLine = () => {
+    const textarea = textareaRef.current;
+    if (!textarea) return;
+
+    const { selectionStart } = textarea;
+    const text = noteContent;
+    const lines = text.split('\n');
+
+    let charCount = 0;
+    let lineIndex = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+      if (charCount + lines[i].length + 1 > selectionStart) {
+        lineIndex = i;
+        break;
+      }
+      charCount += lines[i].length + 1;
+    }
+
+    const line = lines[lineIndex];
+
+    if (line.startsWith('• ')) {
+      lines[lineIndex] = line.replace(/^• /, '');
+    } else {
+      lines[lineIndex] = '• ' + line;
+    }
+
+    const updated = lines.join('\n');
+    setNoteContent(updated);
+
+    setTimeout(() => {
+      const pos = selectionStart + (line.startsWith('• ') ? -2 : 2);
+      textarea.setSelectionRange(pos, pos);
+    }, 0);
+  };
 
   useEffect(() => {
     if (hasUserEdited) {
@@ -159,138 +236,150 @@ export const Note = ({ note, isReadOnly }: NoteProps) => {
   const isWinner = (note.totalVotes ?? 0) === maxVotes && maxVotes > 0;
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger asChild>
-        <div className="relative flex flex-col items-center">
-          <div
-            ref={noteRef}
-            onClick={(e) => {
-              if (
-                (e.target as HTMLElement).tagName.toLowerCase() !== 'textarea'
-              ) {
-                setIsOpen(true);
-              }
-            }}
-            style={{
-              width: `${noteSize.width}px`,
-              height: `${noteSize.height}px`,
-            }}
-            className={clsx(
-              noteColorClassMap[
-                localNoteColor as keyof typeof noteColorClassMap
-              ],
-              'relative w-full border p-3 text-xs cursor-pointer flex flex-col justify-between',
-              selectedNoteId === uuid &&
-                'ring-4 ring-primary/60 shadow-xl scale-[1.02] z-20 animate-pulse-slow',
-              isWinner && 'ring-1 ring-yellow-400',
-            )}
-          >
-            <textarea
-              readOnly={isReadOnly}
-              value={noteContent}
-              onChange={handleNoteContentChange}
-              placeholder="Type in your idea..."
-              className="w-full resize-none h-full max-h-[250px] overflow-y-auto p-2 tracking-wide  border-none outline-none text-sm text-black"
-              aria-label="Note input"
-            />
+    <>
+      <Popover open={isOpen} onOpenChange={setIsOpen}>
+        <PopoverTrigger asChild>
+          <div className="relative flex flex-col items-center">
+            <div
+              ref={noteRef}
+              onClick={(e) => {
+                if (
+                  (e.target as HTMLElement).tagName.toLowerCase() !== 'textarea'
+                ) {
+                  setIsOpen(true);
+                }
+              }}
+              style={{
+                width: `${noteSize.width}px`,
+                height: `${noteSize.height}px`,
+              }}
+              className={clsx(
+                noteColorClassMap[
+                  localNoteColor as keyof typeof noteColorClassMap
+                ],
+                'relative w-full border p-3 text-xs cursor-pointer flex flex-col justify-between',
+                selectedNoteId === uuid &&
+                  'ring-4 ring-primary/60 shadow-xl scale-[1.02] z-20 animate-pulse-slow',
+                isWinner && 'ring-1 ring-yellow-400',
+              )}
+            >
+              <textarea
+                ref={textareaRef}
+                value={noteContent}
+                onChange={handleNoteContentChange}
+                onKeyDown={handleEnterKey}
+                placeholder="Type in your idea..."
+                className="w-full resize-none h-full max-h-[250px] overflow-y-auto p-2 tracking-wide border-none outline-none text-sm text-black"
+              />
 
-            <div className="flex justify-between items-center w-full">
-              <span className="text-gray-700 text-xs ml-[7px]">
-                {author?.firstName || 'Unknown'}
-              </span>
-
-              <div className="flex items-center gap-1 -mr-[15px]">
-                <span
-                  className="text-[11px] font-medium text-blue-800 bg-blue-100 px-2 py-0.5 rounded-full flex items-center gap-1"
-                  title="Total comments"
-                >
-                  <MessageSquare className="w-3 h-3 text-blue-500 fill-blue-300" />
-                  3
+              <div className="flex justify-between items-center w-full">
+                <span className="text-gray-700 text-xs ml-[7px]">
+                  {author?.firstName || 'Unknown'}
                 </span>
 
-                {(note.totalVotes ?? 0) > 0 && (
+                <div className="flex items-center gap-1 -mr-[15px]">
                   <span
-                    className="text-[11px] font-medium text-yellow-800 bg-yellow-100 px-2 py-0.5 rounded-full flex items-center gap-1"
-                    title="Total votes"
+                    className="text-[11px] font-medium text-blue-800 bg-blue-100 px-2 py-0.5 rounded-full flex items-center gap-1"
+                    title="Total comments"
                   >
-                    <Star className="w-3 h-3 text-yellow-500 fill-yellow-300" />
-                    {note.totalVotes}
+                    <MessageSquare className="w-3 h-3 text-blue-500 fill-blue-300" />
+                    3
                   </span>
-                )}
 
-                <div
-                  className="cursor-se-resize w-5 h-5 text-xs -mb-[12px]"
-                  onClick={() => setIsResizing(true)}
-                ></div>
+                  {(note.totalVotes ?? 0) > 0 && (
+                    <span
+                      className="text-[11px] font-medium text-yellow-800 bg-yellow-100 px-2 py-0.5 rounded-full flex items-center gap-1"
+                      title="Total votes"
+                    >
+                      <Star className="w-3 h-3 text-yellow-500 fill-yellow-300" />
+                      {note.totalVotes}
+                    </span>
+                  )}
+
+                  <div
+                    className="cursor-se-resize w-5 h-5 text-xs -mb-[12px]"
+                    onClick={() => setIsResizing(true)}
+                  ></div>
+                </div>
               </div>
             </div>
+
+            {isWinner && (
+              <div className="absolute -top-[19px] -left-[16px] z-50">
+                <Crown className="w-8 h-8 text-amber-300 fill-amber-200 drop-shadow-lg -rotate-37" />
+              </div>
+            )}
           </div>
+        </PopoverTrigger>
 
-          {isWinner && (
-            <div className="absolute -top-[19px] -left-[16px] z-50">
-              <Crown className="w-8 h-8 text-amber-300 fill-amber-200 drop-shadow-lg -rotate-37" />
-            </div>
-          )}
-        </div>
-      </PopoverTrigger>
-
-      <PopoverContent side="top" align="center" sideOffset={10}>
-        <div className="flex items-center justify-center gap-3">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>
-                <X
+        <PopoverContent side="top" align="center" sideOffset={10}>
+          <div className="flex items-center justify-center gap-3">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <X
+                    className="cursor-pointer"
+                    size={20}
+                    strokeWidth={2.5}
+                    onClick={() => !isReadOnly && handleDeleteNote(uuid)}
+                  />
+                </TooltipTrigger>
+                <TooltipContent>Delete</TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger
+                  onClick={toggleBulletOnCurrentLine}
                   className="cursor-pointer"
-                  size={20}
-                  strokeWidth={2.5}
-                  onClick={() => !isReadOnly && handleDeleteNote(uuid)}
-                />
-              </TooltipTrigger>
-              <TooltipContent>Delete</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+                >
+                  <List className="h-5 w-5" />
+                </TooltipTrigger>
+                <TooltipContent>Bulleted list</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
 
-          {fillColors.map((color) => (
-            <Circle
-              key={color}
-              className={clsx(
-                'w-4 h-4 cursor-pointer',
-                localNoteColor === color && 'ring-2 ring-primary rounded-full',
-              )}
-              strokeWidth={2.5}
-              style={{ fill: `var(--${color})` }}
-              onClick={() => !isReadOnly && handleNoteColorChange(color)}
-            />
-          ))}
+            {fillColors.map((color) => (
+              <Circle
+                key={color}
+                className={clsx(
+                  'w-4 h-4 cursor-pointer',
+                  localNoteColor === color &&
+                    'ring-2 ring-primary rounded-full',
+                )}
+                strokeWidth={2.5}
+                style={{ fill: `var(--${color})` }}
+                onClick={() => !isReadOnly && handleNoteColorChange(color)}
+              />
+            ))}
 
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>
-                <div className="flex flex-col items-center">
-                  <Toggle
-                    size="sm"
-                    variant="ghost"
-                    className="cursor-pointer py-2"
-                    onClick={() => !isReadOnly && handleVote()}
-                  >
-                    {hasVoted ? (
-                      <Star className="fill-foreground" />
-                    ) : (
-                      <Star strokeWidth={2.5} />
-                    )}
-                  </Toggle>
-                  <p className="text-xs font-semibold">
-                    {note.totalVotes || 0}
-                  </p>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>Vote / Unvote</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <div className="flex flex-col items-center">
+                    <Toggle
+                      size="sm"
+                      variant="ghost"
+                      className="cursor-pointer py-2"
+                      onClick={() => !isReadOnly && handleVote()}
+                    >
+                      {hasVoted ? (
+                        <Star className="fill-foreground" />
+                      ) : (
+                        <Star strokeWidth={2.5} />
+                      )}
+                    </Toggle>
+                    <p className="text-xs font-semibold">
+                      {note.totalVotes || 0}
+                    </p>
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>Vote / Unvote</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
 
-          <PanelToggle noteId={uuid} />
-        </div>
-      </PopoverContent>
-    </Popover>
+            <PanelToggle noteId={uuid} />
+          </div>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 };
