7cbc7b7d7a6c9ec175486afaa19b8e29d34a4b03
elsatafilajj <elsatafilaj11@gmail.com>
2025-06-24 10:01:46 +0200

    feat:implement redirect after login logic

--- src/components/JoinRoom/index.tsx
+++ src/components/JoinRoom/index.tsx
@@ -4,18 +4,23 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 import { joinRoom } from '@/api/Room/room.client';
 import { queryKeys } from '@/constants/queryKeys';
+import { useAuthContext } from '@/context/AuthContext/AuthContext';
 import { getSocket } from '@/helpers/socket';
 
 export const JoinRoom = () => {
   const queryClient = useQueryClient();
   const { roomId } = useParams<{ roomId: string }>();
+  const params = useParams<{ code: string }>();
   const navigate = useNavigate();
-  const { code } = useParams();
   const socket = getSocket();
+  const { isAuthenticated } = useAuthContext();
+
+  const code = params.code || localStorage.getItem('inviteCode') || '';
 
   const joinRoomMutation = useMutation({
     mutationFn: (code: string) => joinRoom(code),
     onSuccess: (data) => {
+      localStorage.removeItem('inviteCode');
       queryClient.invalidateQueries({
         queryKey: queryKeys.getSingleRoom(roomId || ''),
       });
@@ -24,12 +29,15 @@ export const JoinRoom = () => {
   });
 
   useEffect(() => {
-    joinRoomMutation.mutateAsync(code || '');
+    if (!isAuthenticated || !code) return;
+
+    joinRoomMutation.mutateAsync(code);
     socket.emit('rooms/join', { roomId });
+
     queryClient.invalidateQueries({
       queryKey: queryKeys.getUsers(),
     });
-  }, []);
+  }, [isAuthenticated, code]);
 
   return <div></div>;
 };
